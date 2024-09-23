ca93338691794087b3d97edba7eda1f04afc4fbc
Dermenji-Alex <lololowka1793@gmail.com>
2024-09-23 14:25:27 +0300

    Deploy frontend build

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ const path = require('path');
 
 // В production обслуживаем статические файлы из React
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
